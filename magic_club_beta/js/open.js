921c51a12852be99eb5c4c4fbb86413752b4f11b
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-01-30 01:55:08 +0900

    intro fix
    seperation + add activities

---
+++ magic_club_beta/js/open.js
@@ -0,0 +1,27 @@
+function openIntro() {
+  window.open("/templates/introduction.html", "_self");
+}
+
+function openEvent() {
+  window.open("/templates/event_ongoing.html", "_self");
+}
+
+function openComu() {
+  window.open("https://open.kakao.com/o/g73dHOZf", "_blank");
+}
+
+function openInquire() {
+  window.open("/templates/inquire.html", "_self");
+}
+
+function openNotice() {
+  window.open("/templates/notification.html", "_self");
+}
+
+function openAccount() {
+  window.open("/templates/account.html", "_self");
+}
+
+function openRegitser() {
+  window.open("/templates/register.html", "_self");
+}
