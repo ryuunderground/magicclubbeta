9c9f61ce5237f3ba30ff6faaaa142bcce15120ed
ryuunderground <138040119+ryuunderground@users.noreply.github.com>
2024-01-30 23:15:12 +0900

    notice_rule

--- magic_club_beta/js/open.js
+++ magic_club_beta/js/open.js
@@ -25,3 +25,15 @@ function openAccount() {
 function openRegitser() {
   window.open("/templates/register.html", "_self");
 }
+
+function openLogin() {
+  window.open("/templates/login.html", "_self");
+}
+
+function openRule() {
+  window.open("/templates/notification_rule.html", "_self");
+}
+
+function openTest() {
+  window.open("/templates/notification_contents.html", "_self");
+}
